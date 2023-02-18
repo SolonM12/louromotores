4f5bc32d98748f0de881546e49a77ac1d4054993
SolonM12 <123671325+SolonM12@users.noreply.github.com>
2023-02-18 13:30:44 -0300

    Versão 1.0
    Primeira versão do site reformado da louro, no momento só temos a versão mobile, a versão desktop ainda vai ser configurada. Adicionei um menu de navegação e as principais informações iniciais. como cabeçalho estrutura e etc.

---
+++ script/script.js
@@ -0,0 +1,32 @@
+
+// variáveis do menu
+var menu = document.getElementById('menu');
+var navMenu = document.getElementById('navMenu');
+var a = 'off';
+
+// animação do menu
+function clickMenu()  {
+    if (a == 'off'){
+    menu.style.background = 'var(--azulweg)'
+    navMenu.style.visibility = 'visible'
+    navMenu.style.width = '65%'
+    a = 'on'
+}
+    else {
+        menu.style.background = 'var(--azullouro)'
+        navMenu.style.visibility = 'hidden'
+        navMenu.style.width = '0%'
+        a = 'off'
+    }
+    
+};
+function sair(){
+    menu.style.background = 'var(--azullouro)'
+    navMenu.style.visibility = 'hidden'
+    navMenu.style.width = '0%'
+    a = 'off'
+};
+//aparecer link da weg (apenas mouse enter na div imagem quando desktop)
+function linkWeg(){
+    document.getElementById('wegLink').style.display = 'block';
+}
