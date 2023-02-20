fa1d345eff393a4df5b4a92075044dc70b4900b8
SolonM12 <123671325+SolonM12@users.noreply.github.com>
2023-02-19 22:55:19 -0300

    Primeira versão do site finalizada!!!
    Finalizei a primeira versão do site, ainda vou atualizar para correção de alguns bugs. como o do menu e outros.

--- script/script.js
+++ script/script.js
@@ -4,6 +4,49 @@ var menu = document.getElementById('menu');
 var navMenu = document.getElementById('navMenu');
 var a = 'off';
 
+//ideia do mapa
+//passo 1
+//cliclar num objeto e ele fazer um mapa aparecer (feito)
+//fazer o marcador mudar sua cor quando o mapa aparecer (feito)
+//fazer a mesma coisa com o mapa de belo jardim (feito)
+//fazer com que quando marque um, desmarque o outro
+
+
+
+
+//variaveis mapa Recife
+var mapaRecife = document.getElementById('mapaRecife');
+var marcadorRec = document.getElementById('marcadorRec')
+//variaveis mapa Belo Jardim
+var mapaBelojardim = document.getElementById('mapaBelojardim');
+var marcadorBj = document.getElementById('marcadorBj')
+
+
+
+//por padão recife ligado
+mapaRecife.style.display = 'block'
+marcadorRec.style.background = 'var(--azulweg)'
+//mapa de Belo Jardim
+function toggleMapaBj(){
+    //Tirando o mapa de Recife
+    mapaRecife.style.display = 'none'
+    marcadorRec.style.background = 'white'
+
+    //Adicionando o mapa de Belo Jardim
+    mapaBelojardim.style.display = 'block'
+    marcadorBj.style.background = 'var(--azulweg)'
+}
+
+//mapa de recife
+function toggleMapaR(){
+    //tirando o mapa de Belo Jardim
+    mapaBelojardim.style.display = 'none'
+    marcadorBj.style.background = 'white'
+    //adicionadno o mapa de recife
+    mapaRecife.style.display = 'block'
+    marcadorRec.style.background = 'var(--azulweg)'
+}
+
 // animação do menu
 function clickMenu()  {
     if (a == 'off'){
